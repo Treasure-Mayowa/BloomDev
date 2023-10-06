e7243386503a76f38396a7e206d40e3435ea7d22
Treasure Mayowa <treasuremayowa@outlook.com>
2023-10-06 01:18:19 +0100

    Move Bloom into this repo

---
+++ render/static/render/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    const todoForm = document.querySelector("#todo-form")
+    document.querySelector(".icon").addEventListener("click", ()=> {
+        var x = document.getElementById("nav");
+        if (x.className === "navbar shadow") {
+            x.className += "responsive";
+        } else {
+            x.className = "navbar shadow";
+        }
+    })
+    document.getElementById("new-todo").addEventListener("click", ()=> {
+        if (todoForm.style.display === "none") {
+            todoForm.style.display = "block"
+        } else {
+            todoForm.style.display = "none"
+        }
+    })
+})
