d5cf7d5e11746afad82ce65c7198ed27207bd048
Treasure Mayowa <treasuremayowa@outlook.com>
2023-10-06 01:25:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 poetry install
 pip install gunicorn
-pip install python-decouple
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
