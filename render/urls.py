81ec5cc35e1472c82069c9f233bd803a09c06a8a
Treasure Mayowa <treasuremayowa@outlook.com>
2023-10-06 10:57:52 +0100

    Continue migration

--- render/urls.py
+++ render/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("journal", views.journal, name="journal"),
+    path("journal/add", views.add_journal, name="add_journal"),
+    path("journal/edit/<int:journal_id>", views.edit_journal, name="edit_journal"),
+    path("bloombot", views.bloombot, name="bloombot"),
+    path("todos", views.todos, name="todos"),
+    path("add_todo", views.add_todo, name="add_todo"),
+    path("delete_todo/<int:id>", views.delete_todo, name="delete_todo"),
+    path("privacy-policy", views.privacy_policy, name="privacy-policy"),
+    path("blog", views.blog, name="blog"),
 ]
--- render/views.py
+++ render/views.py
@@ -1,8 +1,99 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import datetime
+from .models import *
+
 
 # Create your views here.
 def index(request):
     return render(request, 'render/index.html', {})
+
+@login_required
+def journal(request):
+    journal_entries = Journal.objects.filter(author=request.user).order_by('-created_at')
+    return render(request, "render/journal.html", {
+        "journals": journal_entries
+    })
+
+def blog(request):
+    return render(request, "render/blog.html")
+
+
+@login_required
+def add_journal(request):
+    prompt = request.GET.get('prompt', '')
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        new_journal = Journal(author=request.user, title=title, content=content)
+        new_journal.save()
+        return HttpResponseRedirect(reverse("journal"))
+    else:
+        return render(request, "render/add_journal.html", {
+            "prompt": prompt
+        })
+
+
+@login_required
+def edit_journal(request, journal_id):
+    try:
+        journal_entry = Journal.objects.get(pk=journal_id)
+    except Journal.DoesNotExist:
+        return HttpResponse("You do not have the permission to edit this journal")
+    if request.user != journal_entry.author:
+        return HttpResponse("You do not have the permission to edit this journal")
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        journal_entry.title = title
+        journal_entry.content = content
+        journal_entry.save()
+        return HttpResponseRedirect(reverse("journal"))
+    return render(request, "render/edit_journal.html", {
+        "journal": journal_entry
+    })
+
+
+@login_required
+def privacy_policy(request):
+    return render(request, "render/privacy-policy.html")
+
+@login_required
+def bloombot(request):
+    return render(request, "render/bloombot.html")
+
+
+@login_required
+def todos(request):
+    todos = Todo.objects.filter(user=request.user, day_added=datetime.date.today())
+    return render(request, "render/todos.html", {
+        "todos": todos
+    })
+
+
+@login_required
+def add_todo(request):
+    if request.method == "POST":
+        todo_item = request.POST["todo"]
+        details = request.POST["details"]
+        new_todo = Todo(user=request.user, name=todo_item, details=details, day_added=datetime.date.today())
+        new_todo.save()
+        return HttpResponseRedirect(reverse("todos"))
+
+
+@login_required
+def delete_todo(request, id):
+    todo = Todo.objects.get(pk=id)
+    if todo.user == request.user:
+        todo.delete()
+        return HttpResponseRedirect(reverse("todos"))
+    else:
+        return HttpResponseRedirect(reverse('todos'))
 
 
 def login_view(request):
