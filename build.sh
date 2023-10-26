d25f84a5a8fb7d2c84eb711501e27b908897f7af
Treasure Mayowa <treasuremayowa@outlook.com>
2023-10-26 16:17:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ pip install gunicorn
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations render
+python manage.py makemigrations
 python manage.py migrate
